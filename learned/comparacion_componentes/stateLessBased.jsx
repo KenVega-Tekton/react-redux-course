d8ffda1c9ad153aae12024d266a4d6ac9fe46f0b
Alexander Vega <vega.ken.developer@gmail.com>
2018-03-22 21:55:44 -0500

    initial commit

---
+++ learned/comparacion_componentes/stateLessBased.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HelloWorld = (props) => {
+  const sayHi = (event) => {
+    alert(`Hi ${props.name}`);
+  }
+
+  return (
+    <div>
+      <a href="#" onClick={sayHi}>Say Hi</a>
+    </div>
+  )
+}
+
+HelloWorld.propTypes = {
+  name = React.propTypes.string.isRequired
+}
+
+export default HelloWorld;
