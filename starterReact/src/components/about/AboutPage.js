bc06cd96656fbb3cc66e132af13022d52ea67632
Ken Vega <ken.vega@tektonlabs.com>
2018-03-23 16:06:04 -0500

    added knowledge before starting to code

---
+++ starterReact/src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class AboutPage extends Component{
+  render(){
+    return (
+      <div className="jumbotron">
+        <h1>About</h1>
+        <p>This app uses React, Redux, React router</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
